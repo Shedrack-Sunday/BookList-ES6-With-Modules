0aa8075c826afc4a68c01e09572fdf866a7a5ef7
Shedrack Sunday <Hannania002@gmail.com>
2022-09-27 09:56:42 +0100

    Add changes to index.js

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ import { DateTime } from './modules/luxon.js';
 import Book from './modules/index.js';
 
 const form = document.querySelector('.form-input');
-const [title, author] = form.elements;
-const [navList, navAdd, navContact] = document.querySelectorAll('.list-item');
 const allBooks = document.querySelector('.all-books');
 const addBook = document.querySelector('.add-book');
 const contact = document.querySelector('.contact');
 const divTime = document.querySelector('.date-time');
+const [title, author] = form.elements;
+const [navList, navAdd, navContact] = document.querySelectorAll('.list-item');
 
+// TIMER DISPLAY, BASED ON LUXON LIBRARAY IN REAL TIME.
 setInterval(() => {
   const dt = DateTime.now();
   divTime.textContent = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 }, 1000);
 
+// Array initiation from book class to store entries from user.
 const inputBook = {};
 const objBook = new Book();
 
@@ -21,6 +23,9 @@ if (localStorage.savedBooks) {
   objBook.books = JSON.parse(localStorage.getItem('savedBooks'));
 }
 
+// Events based on actions of the user on display page
+// The three pages will display indipendently, 
+// based on user actions.
 navList.addEventListener('click', () => {
   allBooks.classList.remove('hidden');
   addBook.classList.add('hidden');
@@ -51,11 +56,12 @@ const populateFields = () => {
   localStorage.setItem('savedBooks', JSON.stringify(objBook.books));
 };
 
+// Form action based on user actions
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   objBook.addBook(new Book(inputBook.title, inputBook.author));
   form.submit();
 });
 
-objBook.displayBooks();
+objBook.itemsDisplay();
 populateFields();
